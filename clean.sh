0027a18e339c2b589a006e90d6a575d4ff4e8b4b
Ahnaf <159417805+ahnaf505@users.noreply.github.com>
2025-04-06 13:10:23 +0000

    Update README to indicate maintenance mode and add clean script for build management

---
+++ clean.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ ! -d "bin" ]; then
+  mkdir bin
+fi
+
+if [ "$(ls -A bin)" ]; then
+  if [ ! -d "archive" ]; then
+    mkdir archive
+  fi
+  mv bin/* archive/
+fi
+
+if [ -f "dist/main.bin" ]; then
+  mv dist/main.bin bin/
+fi
+
+if [ "$(ls -A bin)" ]; then
+  for file in bin/*; do
+    hash=$(sha256sum "$file" | awk '{print $1}')
+    timestamp=$(date +"%Y%m%d%H%M%S")
+    random_number=$((RANDOM))
+    extension="${file##*.}"
+    
+    mv "$file" "bin/Torrent2Direct${hash}_${timestamp}_${random_number}.${extension}"
+  done
+fi
+
+if [ -d "dist" ]; then
+  rm -rf dist
+fi
